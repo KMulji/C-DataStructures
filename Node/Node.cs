df5e367c49d49d03a6d4bde47346767b0cf3eb71
Kyan Mulji <muljikyan@gmail.com>
2024-08-12 15:21:24 +0300

    singly

---
+++ Node/Node.cs
@@ -0,0 +1,25 @@
+﻿namespace Node;
+
+public class Node<T>
+{
+    private T _data;
+    private Node<T>? _next;
+
+    public Node(T data)
+    {
+        _data = data;
+        _next = null;
+    }
+
+    public T Data
+    {
+        get { return _data; }
+        set { _data = value; }
+    }
+    public Node<T>? Next
+    {
+        get { return _next; }
+        set { _next = value; }
+    }
+
+}
---
+++ SinglyList/Singly.cs
@@ -0,0 +1,33 @@
+﻿namespace SinglyList;
+
+using Node;
+public class Singly<T>
+{
+    private Node<T>? _head;
+    private Node<T>? _tail;
+
+    public void Append(T val)
+    {
+        Node<T> temp;
+        if (_head == _tail)
+        {
+            temp = new Node<T>(val);
+            _head = temp;
+            _tail = temp;
+            return;
+        }
+        temp = new Node<T>(val);
+        temp.Next = _head;
+        _head = temp;
+        
+    }
+
+    public Singly()
+    {
+        _head = null;
+        _tail = null;
+    }
+
+
+
+}
